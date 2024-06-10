529cf2cc7016287d13ada903a6d2bf94b22721af
Anshtyagi1729 <138436729+Anshtyagi1729@users.noreply.github.com>
2024-06-10 19:10:59 +0530

    Add files via upload

---
+++ rckPapCis/index.js
@@ -0,0 +1,20 @@
+'use strict';
+const headBoard=document.querySelector('.heading');
+const Cmp=document.querySelector('.comp');
+const player=document.querySelector('.player');
+let playing=true;
+const move0=document.querySelector('.move--0'); 
+const move1=document.querySelector('.move--1'); 
+const newBtn=document.querySelector('btn--new');
+const roBtn=document.querySelector('.btn--rock');
+const papBtn=document.querySelector('.btn--pap');
+const cisBtn=document.getElementsByClassName('btn--cis');
+const shootBtn=document.querySelectorAll('.btn');
+console.log(shootBtn);
+let compmmove=0;
+shootBtn.
+// roBtn.addEventListener('click',function()
+// {
+//     move1.src
+// })
+
